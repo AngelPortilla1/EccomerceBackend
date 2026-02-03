acacc9c196da3ae1c72b68fe43c57ca3a1af5676
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-02-02 23:32:05 -0500

    Check configdb

--- src/config/configdb.js
+++ src/config/configdb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const dbURI = process.env.MONGO_DB_URI.replace('<db_username>',process.env.MONGO_DB_USER)
+        const dbURI = process.env.MONGO_DB_URI.replace('<db_username>',process.env.MONGO_DB_USER).replace('<db_password>',process.env.MONGO_DB_PASSWORD);
     } catch (error){
 
     }
