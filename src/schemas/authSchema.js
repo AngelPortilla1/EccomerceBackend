3063caf80986c596219c91813c33c207732c75ed
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-03-16 22:02:42 -0500

    New schema login add

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -1,8 +1,14 @@
 
+import e from 'express';
 import {z } from 'zod';
 
 export const registerSchema = z.object({
     username: z.string().min(3).max(29),
+    email:z.email().min(6).max(254),
+    password: z.string().min(6).max(254)
+})
+
+export const loginSchema = z.object({
     email:z.email().min(6).max(254),
     password: z.string().min(6).max(254)
 })
