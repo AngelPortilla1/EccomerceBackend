6512be6aad32c34eda2f3ea2fee03b180a328065
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-04-15 00:07:23 -0500

    Fix: endpoint post product

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,25 +5,25 @@ const router = express.Router();
 
 
 //rutas publicas
-router.get('/products', (req, res) => {
+router.get('/', (req, res) => {
     res.json({ message: 'Lista de productos' });
 });
 
-router.get('/products/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const { id } = req.params;
     res.json({ message: `Detalles del producto con ID: ${id}` });
 });
 
 
 //rutas privadas (Solo administradores pueden moidifcar productos))
-router.post('/products', createProduct);
+router.post('/', createProduct);
 
-router.put('/products/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     const { id } = req.params;
     res.json({ message: `Producto con ID: ${id} actualizado` });
 });
 
-router.delete('/products/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     const { id } = req.params;
     res.json({ message: `Producto con ID: ${id} eliminado` });
 });
