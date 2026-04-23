839ce6243c03f4cfa99548a756ea6d0caba96810
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-04-22 22:49:26 -0500

    Update : Since now the model product is added

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,9 +1,25 @@
+
+import ProductModel from '../models/ProductModel.js';
+import { productSchema } from '../schemas/productSchema.js';
+import { ZodError } from 'zod';
+
 export const createProduct = async (req, res) => {
     try{
-        const {name,description,price,stock,imageUrl}= req.body
+        const {name,description,price,stock,imageUrl}= productSchema.parse(req.body);
 
-        res.json({data:{name,description,price,stock,imageUrl}})
+
+        // Create the product in the database
+        const product = await ProductModel.create({
+            name,
+            description,
+            price,
+            stock,
+            imageUrl
+        });
+
+        return res.status(201).json(product);
     }catch (error){
+        res.json({message:'Error al crear el producto'})
 
     }
 
