bed88a113b4a93df20554742c894856df825e20a
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-06-23 23:31:43 -0500

    Fix: eliminar duplicación en la conexión a la base de datos

--- src/server.js
+++ src/server.js
@@ -42,3 +42,12 @@ connectDB().then(() => {
     disconnectDB()
 });
 
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })  
+}).catch((error) => {
+    console.error('Error connecting to the database:', error);
+    disconnectDB();
+}
+
